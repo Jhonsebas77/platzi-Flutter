b3e16c630ea5f8861e55c8183cb67199700d36d3
Sebastian Otalora <sebas@MBP-JSOB.local>
2020-10-17 23:19:19 -0500

    Send the info way Construct

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,10 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final String descriptionDummy =
+      """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus odio ut lorem tincidunt viverra. Suspendisse iaculis congue commodo. In sagittis sollicitudin quam at aliquam. Phasellus finibus nibh a erat tincidunt dignissim. Suspendisse maximus turpis maximus justo egestas, a congue enim tempus. In enim sem, volutpat id massa gravida, pellentesque ullamcorper augue. Cras ut augue eu felis commodo posuere. Nulla facilisi. Nulla a ex in leo elementum vulputate vel sit amet purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis non egestas odio. Nunc tortor purus, molestie nec efficitur eu, feugiat auctor urna.
+
+Pellentesque maximus sem ac dui ultricies, id finibus velit congue. Nullam vulputate ligula porttitor ante feugiat, feugiat mattis lectus hendrerit. Nunc lacinia turpis enim, at ultrices quam ullamcorper vel. Cras ut iaculis nisi. Sed ultrices justo et imperdiet tincidunt. Nulla facilisi. Pellentesque sed aliquet mauris. Cras sagittis porttitor sollicitudin.""";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,7 +22,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Hello World"),
         ),
-        body: new DescriptionPlace(),
+        body: new DescriptionPlace('Miami Resort', 5, descriptionDummy),
       ),
     );
   }
--- lib/widget/description_place.dart
+++ lib/widget/description_place.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class DescriptionPlace extends StatelessWidget {
-  final String textDummy =
-      """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus odio ut lorem tincidunt viverra. Suspendisse iaculis congue commodo. In sagittis sollicitudin quam at aliquam. Phasellus finibus nibh a erat tincidunt dignissim. Suspendisse maximus turpis maximus justo egestas, a congue enim tempus. In enim sem, volutpat id massa gravida, pellentesque ullamcorper augue. Cras ut augue eu felis commodo posuere. Nulla facilisi. Nulla a ex in leo elementum vulputate vel sit amet purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis non egestas odio. Nunc tortor purus, molestie nec efficitur eu, feugiat auctor urna.
-
-Pellentesque maximus sem ac dui ultricies, id finibus velit congue. Nullam vulputate ligula porttitor ante feugiat, feugiat mattis lectus hendrerit. Nunc lacinia turpis enim, at ultrices quam ullamcorper vel. Cras ut iaculis nisi. Sed ultrices justo et imperdiet tincidunt. Nulla facilisi. Pellentesque sed aliquet mauris. Cras sagittis porttitor sollicitudin.""";
+  String namePlace;
+  int starts;
+  String descriptionPlace;
+  DescriptionPlace(this.namePlace, this.starts, this.descriptionPlace);
   @override
   Widget build(BuildContext context) {
     final star = Container(
       margin: EdgeInsets.only(top: 323, right: 3),
       child: Icon(Icons.star, color: Colors.yellow),
     );
+    final starHalf = Container(
+      margin: EdgeInsets.only(top: 323, right: 3),
+      child: Icon(Icons.star_half, color: Colors.yellow),
+    );
+    final starEmpty = Container(
+      margin: EdgeInsets.only(top: 323, right: 3),
+      child: Icon(Icons.star_border, color: Colors.yellow),
+    );
     final descriptionText = Container(
       margin: EdgeInsets.only(left: 20, right: 20, top: 10),
       child: Text(
-        textDummy,
+        descriptionPlace,
         textAlign: TextAlign.justify,
       ),
       alignment: Alignment.centerLeft,
@@ -24,13 +33,13 @@ Pellentesque maximus sem ac dui ultricies, id finibus velit congue. Nullam vulpu
         Container(
           margin: EdgeInsets.only(top: 320.0, left: 20.0, right: 20),
           child: Text(
-            "Duwili Ella",
+            namePlace,
             style: TextStyle(fontSize: 30, fontWeight: FontWeight.w900),
             textAlign: TextAlign.center,
           ),
         ),
         Row(
-          children: <Widget>[star, star, star, star, star],
+          children: <Widget>[star, starHalf, star, starEmpty, star],
         )
       ],
     );

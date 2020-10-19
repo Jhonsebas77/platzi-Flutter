b4ee0cf7f3cda16320a2fbfeda953f6f4bc4f28c
Sebastian Otalora <sebas@MBP-JSOB.local>
2020-10-18 21:26:20 -0500

    Add the Review in a Column List

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import './widget/description_place.dart';
-import './widget/review.dart';
+import './widget/review_list.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,10 +10,6 @@ void main() {
 class MyApp extends StatelessWidget {
   String descriptionDummy =
       '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus odio ut lorem tincidunt viverra. Suspendisse iaculis congue commodo. In sagittis sollicitudin quam at aliquam. Phasellus finibus nibh a erat tincidunt dignissim. Suspendisse maximus turpis maximus justo egestas, a congue enim tempus. In enim sem, volutpat id massa gravida, pellentesque ullamcorper augue.''';
-  String pathImage = "src/Assets/Images/photo_jsob.png";
-  String nameUser = 'Sebastian Otalora';
-  String details = '1 Review 5 photos';
-  String comment = 'There is a Amazing Place Here';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,7 +27,7 @@ class MyApp extends StatelessWidget {
               child: Column(
                 children: [
                   new DescriptionPlace('Miami Resort', 5, descriptionDummy),
-                  Review(pathImage, nameUser, details, comment)
+                  ReviewList(),
                 ],
               ))),
     );

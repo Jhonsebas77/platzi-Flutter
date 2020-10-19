4af919354059eb404e13c9275a57a4eb96a3966c
Sebastian Otalora <sebas@MBP-JSOB.local>
2020-10-18 20:14:03 -0500

    Add user info

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import './widget/description_place.dart';
+import './widget/review.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+// ignore: must_be_immutable
 class MyApp extends StatelessWidget {
-  final String descriptionDummy =
-      """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus odio ut lorem tincidunt viverra. Suspendisse iaculis congue commodo. In sagittis sollicitudin quam at aliquam. Phasellus finibus nibh a erat tincidunt dignissim. Suspendisse maximus turpis maximus justo egestas, a congue enim tempus. In enim sem, volutpat id massa gravida, pellentesque ullamcorper augue. Cras ut augue eu felis commodo posuere. Nulla facilisi. Nulla a ex in leo elementum vulputate vel sit amet purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis non egestas odio. Nunc tortor purus, molestie nec efficitur eu, feugiat auctor urna.
-
-Pellentesque maximus sem ac dui ultricies, id finibus velit congue. Nullam vulputate ligula porttitor ante feugiat, feugiat mattis lectus hendrerit. Nunc lacinia turpis enim, at ultrices quam ullamcorper vel. Cras ut iaculis nisi. Sed ultrices justo et imperdiet tincidunt. Nulla facilisi. Pellentesque sed aliquet mauris. Cras sagittis porttitor sollicitudin.""";
+  String descriptionDummy =
+      '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus odio ut lorem tincidunt viverra. Suspendisse iaculis congue commodo. In sagittis sollicitudin quam at aliquam. Phasellus finibus nibh a erat tincidunt dignissim. Suspendisse maximus turpis maximus justo egestas, a congue enim tempus. In enim sem, volutpat id massa gravida, pellentesque ullamcorper augue.''';
+  String pathImage = "src/Assets/Images/photo_jsob.png";
+  String nameUser = 'Sebastian Otalora';
+  String details = '1 Review 5 photos';
+  String comment = 'There is a Amazing Place Here';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,11 +23,15 @@ Pellentesque maximus sem ac dui ultricies, id finibus velit congue. Nullam vulpu
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello World"),
-        ),
-        body: new DescriptionPlace('Miami Resort', 5, descriptionDummy),
-      ),
+          appBar: AppBar(
+            title: Text("Hello World"),
+          ),
+          body: Column(
+            children: [
+              new DescriptionPlace('Miami Resort', 5, descriptionDummy),
+              Review(pathImage, nameUser, details, comment)
+            ],
+          )),
     );
   }
 }
--- lib/widget/review.dart
+++ lib/widget/review.dart
@@ -3,8 +3,44 @@ import 'package:flutter/material.dart';
 // ignore: must_be_immutable
 class Review extends StatelessWidget {
   String pathImage = "src/Assets/Images/photo_jsob.png";
+  String nameUser = 'Sebastian Otalora';
+  String details = '1 Review 5 photos';
+  String comment = 'There is a Amatzing Place Here';
+
+  Review(this.pathImage, this.nameUser, this.details, this.comment);
+
   @override
   Widget build(BuildContext context) {
+    final userComment = Container(
+      margin: EdgeInsets.only(
+        left: 20,
+      ),
+      child: Text(comment,
+          textAlign: TextAlign.left,
+          style: TextStyle(
+              fontSize: 13, fontFamily: 'lato', fontWeight: FontWeight.w900)),
+    );
+    final userInfo = Container(
+      margin: EdgeInsets.only(
+        left: 20,
+      ),
+      child: Text(details,
+          textAlign: TextAlign.left,
+          style:
+              TextStyle(fontSize: 13, fontFamily: 'lato', color: Colors.grey)),
+    );
+    final userName = Container(
+      margin: EdgeInsets.only(
+        left: 20,
+      ),
+      child: Text(nameUser,
+          textAlign: TextAlign.left,
+          style: TextStyle(fontSize: 17, fontFamily: 'lato')),
+    );
+    final userDetails = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[userName, userInfo, userComment],
+    );
     final photo = Container(
         margin: EdgeInsets.only(top: 20, left: 20),
         width: 80,
@@ -14,7 +50,7 @@ class Review extends StatelessWidget {
             image: DecorationImage(
                 fit: BoxFit.cover, image: AssetImage(pathImage))));
     return Row(
-      children: <Widget>[],
+      children: <Widget>[photo, userDetails],
     );
   }
 }

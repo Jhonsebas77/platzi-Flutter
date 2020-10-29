9b041091345296e7f80e3813ea01cd3824692fb7
Sebastian Otalora <sebas@MBP-JSOB.local>
2020-10-29 18:27:38 -0500

    Fix a issue with the logout

--- lib/User/ui/screens/sign_in_screen.dart
+++ lib/User/ui/screens/sign_in_screen.dart
@@ -78,6 +78,7 @@ class _SignInScreenState extends State<SignInScreen> {
               ButtonGreen(
                 text: 'Login with Gmail',
                 onPressed: () {
+                  userBloc.signOut();
                   userBloc.signIn().then(
                       (FirebaseUser user) => print("El usuario es $user"));
                 },

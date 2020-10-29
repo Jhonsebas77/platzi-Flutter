0bab293195ac532d73a628502e9dff46f2fdc85c
Sebastian Otalora <sebas@MBP-JSOB.local>
2020-10-29 10:14:17 -0500

    Implement the logout button

--- lib/User/ui/widgets/button_bar.dart
+++ lib/User/ui/widgets/button_bar.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzi_trips_app/User/bloc/bloc_user.dart';
 import 'circle_button.dart';
 
 class ButtonsBar extends StatelessWidget {
+  UserBloc userBloc;
   @override
   Widget build(BuildContext context) {
+    userBloc = BlocProvider.of(context);
     return Padding(
         padding: EdgeInsets.symmetric(horizontal: 0.0, vertical: 10.0),
         child: Row(
@@ -13,7 +17,7 @@ class ButtonsBar extends StatelessWidget {
             CircleButton(false, Icons.add, 40.0,
                 Color.fromRGBO(255, 255, 255, 1), () => {}),
             CircleButton(true, Icons.exit_to_app_rounded, 20.0,
-                Color.fromRGBO(255, 255, 255, 1), () => {})
+                Color.fromRGBO(255, 255, 255, 1), () => {userBloc.signOut()})
           ],
         ));
   }

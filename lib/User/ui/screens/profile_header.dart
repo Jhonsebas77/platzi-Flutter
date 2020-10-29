5dc52036ee0d2f255bb2a9c1c760a90ea784e17a
Sebastian Otalora <sebas@MBP-JSOB.local>
2020-10-29 13:53:35 -0500

    Use the profile info that firebase return

--- lib/User/ui/screens/profile_header.dart
+++ lib/User/ui/screens/profile_header.dart
@@ -1,31 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzi_trips_app/User/bloc/bloc_user.dart';
+import 'package:platzi_trips_app/User/model/model.dart';
 import 'package:platzi_trips_app/User/ui/widgets/user_info.dart';
 import 'package:platzi_trips_app/User/ui/widgets/button_bar.dart';
 
 class ProfileHeader extends StatelessWidget {
+  UserBloc userBloc;
+  User user;
   @override
   Widget build(BuildContext context) {
-    final title = Text(
-      'Profile',
-      style: TextStyle(
-          fontFamily: 'Lato',
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-          fontSize: 30.0),
+    userBloc = BlocProvider.of<UserBloc>(context);
+    return StreamBuilder(
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+          case ConnectionState.none:
+            return CircularProgressIndicator();
+          case ConnectionState.active:
+          case ConnectionState.done:
+            return showProfileInfo(snapshot);
+        }
+      },
+      stream: userBloc.streamFirebase,
     );
+  }
 
-    return Container(
-      margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 50.0),
-      child: Column(
-        children: <Widget>[
-          Row(
-            children: <Widget>[title],
-          ),
-          UserInfo('src/Assets/Images/photo_jsob.png', 'Sebastian Otalora',
-              'jsob@platzi.com'),
-          ButtonsBar()
-        ],
-      ),
-    );
+  Widget showProfileInfo(AsyncSnapshot snapshot) {
+    if (!snapshot.hasData || snapshot.hasError) {
+      return Container(
+        margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 50.0),
+        child: Column(
+          children: <Widget>[
+            CircularProgressIndicator(),
+            Text('Inicia sesion...')
+          ],
+        ),
+      );
+    } else {
+      print('-: LOG :-');
+      user = User(
+          name: snapshot.data.displayName,
+          email: snapshot.data.email,
+          photoURL: snapshot.data.photoUrl);
+      final title = Text(
+        'Profile',
+        style: TextStyle(
+            fontFamily: 'Lato',
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 30.0),
+      );
+      return Container(
+        margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 50.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[title],
+            ),
+            UserInfo(user),
+            ButtonsBar()
+          ],
+        ),
+      );
+    }
   }
 }

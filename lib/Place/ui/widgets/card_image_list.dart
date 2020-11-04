05e29adb6e986d67b1387f2f86b57fecb0d420b0
Sebastian Otalora <sebas@MBP-JSOB.local>
2020-11-03 21:04:54 -0500

    Unify the dimensions

--- lib/Place/ui/widgets/card_image_list.dart
+++ lib/Place/ui/widgets/card_image_list.dart
@@ -4,6 +4,9 @@ import 'card_image_with_flaBtn.dart';
 class CardImageList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double width = 300;
+    double height = 250;
+    double left = 20;
     return Container(
       height: 350,
       child: ListView(
@@ -11,35 +14,40 @@ class CardImageList extends StatelessWidget {
         scrollDirection: Axis.horizontal,
         children: <Widget>[
           CardImageWithFabIcon(
-              pathImage: 'src/Assets/Images/IMG_1.jpg',
-              iconData: Icons.favorite_border,
-              height: 250,
-              width: 300,
-              left: 20),
+            pathImage: 'src/Assets/Images/IMG_1.jpg',
+            iconData: Icons.favorite_border,
+            height: height,
+            width: width,
+            left: left,
+          ),
           CardImageWithFabIcon(
-              pathImage: 'src/Assets/Images/IMG_2.jpg',
-              iconData: Icons.favorite_border,
-              height: 250,
-              width: 300,
-              left: 20),
+            pathImage: 'src/Assets/Images/IMG_2.jpg',
+            iconData: Icons.favorite_border,
+            height: height,
+            width: width,
+            left: left,
+          ),
           CardImageWithFabIcon(
-              pathImage: 'src/Assets/Images/IMG_3.jpg',
-              iconData: Icons.favorite_border,
-              height: 250,
-              width: 300,
-              left: 20),
+            pathImage: 'src/Assets/Images/IMG_3.jpg',
+            iconData: Icons.favorite_border,
+            height: height,
+            width: width,
+            left: left,
+          ),
           CardImageWithFabIcon(
-              pathImage: 'src/Assets/Images/IMG_4.jpg',
-              iconData: Icons.favorite_border,
-              height: 250,
-              width: 300,
-              left: 20),
+            pathImage: 'src/Assets/Images/IMG_4.jpg',
+            iconData: Icons.favorite_border,
+            height: height,
+            width: width,
+            left: left,
+          ),
           CardImageWithFabIcon(
-              pathImage: 'src/Assets/Images/IMG_5.jpg',
-              iconData: Icons.favorite_border,
-              height: 250,
-              width: 300,
-              left: 20)
+            pathImage: 'src/Assets/Images/IMG_5.jpg',
+            iconData: Icons.favorite_border,
+            height: height,
+            width: width,
+            left: left,
+          )
         ],
       ),
     );

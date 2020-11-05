5eed2839b78d3d64bfd80a28bc135c476e52d3c1
Sebastian Otalora <sebas@MBP-JSOB.local>
2020-11-05 15:23:12 -0500

    Implements the option to choose from where get the image

--- lib/User/ui/widgets/button_bar.dart
+++ lib/User/ui/widgets/button_bar.dart
@@ -11,7 +11,6 @@ class ButtonsBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     userBloc = BlocProvider.of(context);
-    final picker = ImagePicker();
     return Padding(
         padding: EdgeInsets.symmetric(horizontal: 0.0, vertical: 10.0),
         child: Row(
@@ -19,25 +18,94 @@ class ButtonsBar extends StatelessWidget {
             CircleButton(true, Icons.person, 20.0,
                 Color.fromRGBO(255, 255, 255, 1), () => {}),
             CircleButton(
-                false, Icons.add, 40.0, Color.fromRGBO(255, 255, 255, 1), () {
-              picker
-                  .getImage(
-                source: ImageSource.camera,
-                maxWidth: 640,
-                maxHeight: 480,
-                imageQuality: 50,
-              )
-                  .then((PickedFile image) {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) =>
-                            AddPlaceScreen(image: File(image.path))));
-              }).catchError((onError) => print(onError));
-            }),
+                false,
+                Icons.add,
+                40.0,
+                Color.fromRGBO(255, 255, 255, 1),
+                () => {showPickDialog(context)}),
             CircleButton(true, Icons.exit_to_app_rounded, 20.0,
                 Color.fromRGBO(255, 255, 255, 1), () => {userBloc.signOut()})
           ],
         ));
   }
+}
+
+Future<void> showPickDialog(BuildContext context) async {
+  final picker = ImagePicker();
+  return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+            'Toma una fotografía de tu cámara o elige una de tu galería',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Lato',
+              fontSize: 16,
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+                child: Text(
+                  'Cámara',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Lato',
+                      fontSize: 14),
+                ),
+                onPressed: () {
+                  picker
+                      .getImage(
+                    source: ImageSource.camera,
+                    maxWidth: 640,
+                    maxHeight: 480,
+                    imageQuality: 50,
+                  )
+                      .then((PickedFile image) {
+                    Navigator.pop(context);
+                    if (image != null) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (BuildContext context) => AddPlaceScreen(
+                            image: File(image.path),
+                          ),
+                        ),
+                      );
+                    }
+                  }).catchError((onError) => print(onError));
+                }),
+            FlatButton(
+                child: Text(
+                  'Galería',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Lato',
+                      fontSize: 14),
+                ),
+                onPressed: () {
+                  picker
+                      .getImage(
+                    source: ImageSource.gallery,
+                    maxWidth: 640,
+                    maxHeight: 480,
+                    imageQuality: 50,
+                  )
+                      .then((PickedFile image) {
+                    Navigator.pop(context);
+                    if (image != null) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (BuildContext context) => AddPlaceScreen(
+                            image: File(image.path),
+                          ),
+                        ),
+                      );
+                    }
+                  }).catchError((onError) => print(onError));
+                })
+          ],
+        );
+      });
 }

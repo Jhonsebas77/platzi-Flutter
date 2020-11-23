76c636aaa823b4b46a918aa9d86f359ac5bc1b70
Sebastian Otalora <sebastian.otarola@almundo.com>
2020-11-23 18:42:34 -0500

    Refactor the widgets

--- lib/Search/ui/widget/TripCard.dart
+++ lib/Search/ui/widget/TripCard.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:platzi_trips_app/Search/ui/widget/productIcon.dart';
+import 'package:platzi_trips_app/Search/ui/widget/itemCard.dart';
 
 class TripCard extends StatelessWidget {
   final String destination;
@@ -19,120 +19,75 @@ class TripCard extends StatelessWidget {
   });
   @override
   Widget build(BuildContext context) {
-    double width = 328;
-    double height = 230;
-    double left = 16;
-    double right = 16;
     Alignment initialPosition = Alignment.topCenter;
     Alignment finalPosition = Alignment.bottomCenter;
-    final card = Container(
-        height: height,
-        width: width,
-        margin: EdgeInsets.only(
-          left: left,
-          right: right,
-          bottom: 16,
+    final decorationImage = BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage(pathImage),
         ),
-        decoration: BoxDecoration(
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage(pathImage),
-            ),
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-            shape: BoxShape.rectangle,
-            boxShadow: <BoxShadow>[
-              BoxShadow(
-                color: Colors.black12,
-                blurRadius: 15,
-                offset: Offset(0, 7),
-              )
-            ]));
-    final gradient = Container(
-        height: height,
-        width: width,
-        margin: EdgeInsets.only(
-          left: left,
-          right: right,
-          bottom: 16,
-        ),
-        child: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.only(
-                top: 16,
-                left: 16,
-                bottom: 122,
-              ),
-              child: renderProductList(productList),
-            ),
-            destinationTrip(destination),
-            datesTrip(initDate, finalDate)
-          ],
-        ),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-            shape: BoxShape.rectangle,
-            gradient: LinearGradient(colors: [
-              Colors.transparent,
-              Colors.transparent,
-              Colors.black12,
-              Colors.black45,
-            ], begin: initialPosition, end: finalPosition),
-            boxShadow: <BoxShadow>[
-              BoxShadow(
-                color: Colors.black12,
-                blurRadius: 15,
-                offset: Offset(0, 7),
-              )
-            ]));
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        shape: BoxShape.rectangle,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 15,
+            offset: Offset(0, 7),
+          )
+        ]);
+    final cardImage = renderCardWithDecoration(decorationImage);
+    final colorGradientGray = LinearGradient(
+      colors: [
+        Colors.transparent,
+        Colors.transparent,
+        Colors.black12,
+        Colors.black45,
+      ],
+      begin: initialPosition,
+      end: finalPosition,
+    );
+    final decorationGradient = BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        shape: BoxShape.rectangle,
+        gradient: colorGradientGray,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 15,
+            offset: Offset(0, 7),
+          )
+        ]);
+    final cardGradient = renderCardWithDecoration(decorationGradient);
     return InkWell(
         onTap: function,
         child: Stack(
           alignment: Alignment(0.9, 1.1),
           children: <Widget>[
-            card,
-            gradient,
+            cardImage,
+            cardGradient,
+            ItemCard(
+                destination: destination,
+                initDate: initDate,
+                finalDate: finalDate,
+                productList: productList),
           ],
         ));
   }
 }
 
-Widget renderProductList(List<String> productList) {
-  List<ProductIcon> productIconList = List<ProductIcon>();
-  productList.forEach((p) {
-    productIconList.add(ProductIcon(product: p));
-  });
+Widget renderCardWithDecoration(BoxDecoration decoration) {
+  double width = 328;
+  double height = 230;
+  double left = 16;
+  double right = 16;
   return Container(
-    child: Row(
-      children: productIconList,
+    height: height,
+    width: width,
+    margin: EdgeInsets.only(
+      left: left,
+      right: right,
+      bottom: 16,
     ),
+    decoration: decoration,
   );
-}
-
-Widget datesTrip(String initDate, String finalDate) {
-  return Container(
-      margin: EdgeInsets.only(left: 16),
-      alignment: Alignment.bottomLeft,
-      child: Text(
-        '$initDate - $finalDate',
-        style: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.w700,
-          fontSize: 20,
-        ),
-      ));
-}
-
-Widget destinationTrip(String destination) {
-  return Container(
-      margin: EdgeInsets.only(left: 16),
-      alignment: Alignment.bottomLeft,
-      child: Text(
-        destination,
-        style: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.w700,
-          fontSize: 20,
-        ),
-      ));
 }
---
+++ lib/Search/ui/widget/itemCard.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:platzi_trips_app/Search/ui/widget/productIcon.dart';
+
+class ItemCard extends StatelessWidget {
+  final String destination;
+  final String initDate;
+  final String finalDate;
+  final List<String> productList;
+
+  const ItemCard({
+    Key key,
+    @required this.destination,
+    @required this.initDate,
+    @required this.finalDate,
+    @required this.productList,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    double width = 328;
+    double height = 230;
+    double left = 16;
+    double right = 16;
+    return Container(
+      height: height,
+      width: width,
+      margin: EdgeInsets.only(
+        left: left,
+        right: right,
+        bottom: 16,
+      ),
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(
+              top: 16,
+              left: 16,
+              bottom: 122,
+            ),
+            child: renderProductList(productList),
+          ),
+          destinationTrip(destination),
+          datesTrip(initDate, finalDate)
+        ],
+      ),
+    );
+  }
+}
+
+Widget renderProductList(List<String> productList) {
+  List<ProductIcon> productIconList = List<ProductIcon>();
+  productList.forEach((p) {
+    productIconList.add(ProductIcon(product: p));
+  });
+  return Container(
+    child: Row(
+      children: productIconList,
+    ),
+  );
+}
+
+Widget datesTrip(String initDate, String finalDate) {
+  return Container(
+      margin: EdgeInsets.only(left: 16),
+      alignment: Alignment.bottomLeft,
+      child: Text(
+        '$initDate - $finalDate',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.w700,
+          fontSize: 20,
+        ),
+      ));
+}
+
+Widget destinationTrip(String destination) {
+  return Container(
+      margin: EdgeInsets.only(left: 16),
+      alignment: Alignment.bottomLeft,
+      child: Text(
+        destination,
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.w700,
+          fontSize: 20,
+        ),
+      ));
+}
